719d30ae0215dee65be23cc0a6ef11ea29290ed0
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-25 15:52:28 +0530

    Enhance admin dashboard components with RouterOutlet integration and improve layout styles

--- src/app/page/admin/dashboard/package/packages.ts
+++ src/app/page/admin/dashboard/package/packages.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-packages',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './packages.html',
   styleUrl: './packages.css',
 })
